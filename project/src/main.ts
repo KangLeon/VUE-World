98461b759c2f2893771d5480740ac15988b1f0f3
JY <397879704@qq.com>
2024-04-03 12:52:57 +0800

    页面大致有了，可以搞软著了

--- project/src/main.ts
+++ project/src/main.ts
@@ -2,16 +2,21 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:13:40
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 12:40:38
+ * @LastEditTime: 2024-04-03 12:49:02
  * @FilePath: /project/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from 'vue'
 import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue';
 import { setupRouter } from './router'
 
 function pageConstructor() { 
     const app = createApp(App)
+    app.use(ArcoVue, {
+        // 用于改变使用组件时的前缀名称
+        componentPrefix: 'arco'
+    });
 
     //配置router
     setupRouter(app)
--- project/src/views/login/login.vue
+++ project/src/views/login/login.vue
@@ -2,24 +2,49 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:28:25
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 12:42:08
+ * @LastEditTime: 2024-04-03 12:49:29
  * @FilePath: /project/src/views/login.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
     <div>
-        登录
+        <a-form :model="form" :style="{ width: '600px' }" @submit="handleSubmit">
+            <a-form-item field="name" tooltip="Please enter username" label="Username">
+                <a-input v-model="form.name" placeholder="please enter your username..." />
+            </a-form-item>
+            <a-form-item field="post" label="Post">
+                <a-input v-model="form.post" placeholder="please enter your post..." />
+            </a-form-item>
+            <a-form-item field="isRead">
+                <a-checkbox v-model="form.isRead"> I have read the manual </a-checkbox>
+            </a-form-item>
+            <a-form-item>
+                <a-button html-type="submit">Submit</a-button>
+            </a-form-item>
+        </a-form>
+        {{ form }}
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
     setup() {
+        const form = reactive({
+            name: '',
+            post: '',
+            isRead: false,
+        });
+        const handleSubmit = (data: any) => {
+            console.log(data);
+        };
 
+        return {
+            form,
+            handleSubmit,
+        };
 
-        return {}
     }
 })
 </script>
