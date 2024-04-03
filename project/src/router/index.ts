cc2d07f1358d8c31751e871114d299ac64eaf311
JY <397879704@qq.com>
2024-04-03 15:18:42 +0800

    new

--- project/src/router/index.ts
+++ project/src/router/index.ts
@@ -2,13 +2,14 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:27:51
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 02:39:51
+ * @LastEditTime: 2024-04-03 15:14:12
  * @FilePath: /project/src/router/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { App } from 'vue';
 import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
 
+//TODO：添加很多
 const routes = [{
     path: '/login',
     component: () => import('@/views/login/login.vue'),
