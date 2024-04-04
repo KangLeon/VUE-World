27692e294c61188afad20528c33a214771b021b9
JY <397879704@qq.com>
2024-04-04 12:42:41 +0800

    new

--- project/src/views/login/login.vue
+++ project/src/views/login/login.vue
@@ -2,27 +2,32 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:28:25
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 14:44:14
+ * @LastEditTime: 2024-04-04 12:39:56
  * @FilePath: /project/src/views/login.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-    <div class="p-10 flex flex-col justify-center items-center h-full">
-        <a-form :model="form" :style="{ width: '600px' }" @submit="handleSubmit">
-            <a-form-item field="username" tooltip="Please enter username" label="用户名">
-                <a-input v-model="form.username" placeholder="请输入您的用户名..." />
-            </a-form-item>
-            <a-form-item field="password" tooltip="Please enter password" label="密码 ">
-                <a-input v-model="form.password" placeholder="请输入您的密码..." />
-            </a-form-item>
-            <a-form-item field="isRead">
-                <a-checkbox v-model="form.isRead"> 登录即表示同意隐私协议 </a-checkbox>
-            </a-form-item>
-            <a-form-item>
-                <a-button html-type="submit">提交</a-button>
-            </a-form-item>
-        </a-form>
-        {{ form }}
+    <div class=" bg-blue-200 w-screen h-screen flex flex-col justify-center items-center">
+        <h1>登录到系统</h1>
+        <div class=" p-6 shadow-2xl bg-white rounded-lg">
+            <a-form :model="form" :style="{ width: '400px'}" @submit="handleSubmit">
+                <a-form-item field="username" tooltip="Please enter username" label="用户名">
+                    <a-input v-model="form.username" placeholder="请输入您的用户名..." />
+                </a-form-item>
+                <a-form-item field="password" tooltip="Please enter password" label="密码 ">
+                    <a-input v-model="form.password" placeholder="请输入您的密码..." />
+                </a-form-item>
+                <a-form-item field="isRead">
+                    <a-checkbox v-model="form.isRead"> 登录即表示同意隐私协议 </a-checkbox>
+                </a-form-item>
+                <a-form-item>
+                    <a-button html-type="submit">登录</a-button>
+                    <RouterLink to="/reg">
+                        <a-button class="ml-10" html-type="submit">注册</a-button>
+                    </RouterLink>
+                </a-form-item>
+            </a-form>
+        </div>
     </div>
 </template>
 
