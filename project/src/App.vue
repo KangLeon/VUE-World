7ac48de256adc078685449239f94765df4f4a57d
JY <397879704@qq.com>
2024-04-04 14:17:15 +0800

    new

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,15 +2,17 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:13:40
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 15:07:13
+ * @LastEditTime: 2024-04-04 12:44:56
  * @FilePath: /project/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
+
 </script>
 
 <template>
-  <router-view></router-view>
+  <RouterView></RouterView>>
 </template>
 
 <style scoped></style>
--- project/src/router/index.ts
+++ project/src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:27:51
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-03 15:14:12
+ * @LastEditTime: 2024-04-04 14:14:38
  * @FilePath: /project/src/router/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -16,6 +16,14 @@ const routes = [{
 },{
     path: '/reg',
     component: () => import('@/views/login/reg.vue'),
+}, {
+    path: "/home",
+    component: () => import('@/layout/default.vue'),
+    redirect: 'home/dashboard',
+    children: [{
+        path: '/dashboard',
+        component: () => import('@/views/dashboard/index.vue')
+    }]
 }] as RouteRecordRaw[];
 
 const router = createRouter({
