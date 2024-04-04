07f1a50b97218357fb68efb562216c9e1708cd11
JY <397879704@qq.com>
2024-04-04 16:24:33 +0800

    new

--- project/src/components/sidebarNav/SiderBarNav.vue
+++ project/src/components/sidebarNav/SiderBarNav.vue
@@ -2,7 +2,7 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-04 15:08:57
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-04 15:19:46
+ * @LastEditTime: 2024-04-04 16:24:14
  * @FilePath: /project/src/components/sidebarNav/SiderBarNav.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,24 +10,44 @@
     <div>
         <a-menu :style="{ width: '200px', height: '100%' }" :default-open-keys="['0']" :default-selected-keys="['0_2']"
             show-collapse-button breakpoint="xl" @collapse="onCollapse">
-            <a-sub-menu key="0">
+            <a-menu-item key="0">
                 <template #icon><icon-apps></icon-apps></template>
-                <template #title>Navigation 1</template>
-                <a-menu-item key="0_0">Menu 1</a-menu-item>
-                <a-menu-item key="0_1">Menu 2</a-menu-item>
-                <a-menu-item key="0_2">Menu 3</a-menu-item>
-                <a-menu-item key="0_3">Menu 4</a-menu-item>
-            </a-sub-menu>
+                <RouterLink to="/dashboard">
+                    首页
+                </RouterLink>
+            </a-menu-item>
             <a-sub-menu key="1">
-                <template #icon><icon-bug></icon-bug></template>
-                <template #title>Navigation 2</template>
+                <template #icon><icon-user></icon-user></template>
+                <template #title>
+                    <RouterLink to="/users">
+                        用户管理
+                    </RouterLink>
+                </template>
                 <a-menu-item key="1_0">Menu 1</a-menu-item>
                 <a-menu-item key="1_1">Menu 2</a-menu-item>
                 <a-menu-item key="1_2">Menu 3</a-menu-item>
             </a-sub-menu>
             <a-sub-menu key="2">
-                <template #icon><icon-bulb></icon-bulb></template>
-                <template #title>Navigation 3</template>
+                <template #icon><icon-bulb /></template>
+                <template #title>
+                    <RouterLink to="/roles">
+                        角色管理
+                    </RouterLink>
+                </template>
+                <a-menu-item key="2_0">Menu 1</a-menu-item>
+                <a-menu-item key="2_1">Menu 2</a-menu-item>
+                <a-sub-menu key="2_2" title="Navigation 4">
+                    <a-menu-item key="2_2_0">Menu 1</a-menu-item>
+                    <a-menu-item key="2_2_1">Menu 2</a-menu-item>
+                </a-sub-menu>
+            </a-sub-menu>
+            <a-sub-menu key="3">
+                <template #icon><icon-menu /></template>
+                <template #title>
+                    <RouterLink to="/menus">
+                        菜单管理
+                    </RouterLink>
+                </template>
                 <a-menu-item key="2_0">Menu 1</a-menu-item>
                 <a-menu-item key="2_1">Menu 2</a-menu-item>
                 <a-sub-menu key="2_2" title="Navigation 4">
@@ -41,9 +61,20 @@
 
 <script lang="ts">
 import { Message } from '@arco-design/web-vue'
-import { defineComponent } from 'vue'
+import { PropType, defineComponent } from 'vue'
+
+interface MenuItem { 
+    name: string
+    path: string
+}
 
 export default defineComponent({
+    props: {
+        menus: {
+            type: Array as PropType<Array<MenuItem>>,
+            default: () => [] as MenuItem[],
+        }
+    },
     setup () {
         return {
             onCollapse(_val: any, type: any) {
--- project/src/router/index.ts
+++ project/src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: JY 397879704@qq.com
  * @Date: 2024-04-03 02:27:51
  * @LastEditors: JY 397879704@qq.com
- * @LastEditTime: 2024-04-04 14:40:43
+ * @LastEditTime: 2024-04-04 16:01:41
  * @FilePath: /project/src/router/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,6 +22,15 @@ const routes = [{
     children: [{
         path: 'dashboard',
         component: () => import('@/views/dashboard/index.vue')
+    },{
+        path: 'users',
+        component: () => import('@/views/users/index.vue')
+    },{
+        path: 'menus',
+        component: () => import('@/views/menus/index.vue')
+    },{
+        path: 'roles',
+        component: () => import('@/views/roles/index.vue')
     }]
 }] as RouteRecordRaw[];
 
